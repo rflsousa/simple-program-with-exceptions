7510c529bfdcfb6ea12cf3ea1f5b27d910a649f0
Rafael Sousa <rafael.oliveira.osv@gmail.com>
2019-03-25 08:02:11 -0300

    Projeto sem tratamento de excessões

--- src/model/entities/Account.java
+++ src/model/entities/Account.java
@@ -1,5 +1,53 @@
 package model.entities;
 
-public class Account {
+public final class Account {
+	private Integer number;
+	private String holder;
+	private Double balance;
+	private Double withdrawLimit;
+	
+	public Account(Integer number, String holder, Double balance, Double withdrawLimit) {
+		this.number = number;
+		this.holder = holder;
+		this.balance = balance;
+		this.withdrawLimit = withdrawLimit;
+	}
 
+	public Integer getNumber() {
+		return number;
+	}
+
+	public String getHolder() {
+		return holder;
+	}
+
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+	public Double getWithdrawLimit() {
+		return withdrawLimit;
+	}
+	
+	public void deposit(double amount) {
+		balance += amount;
+	}
+	
+	public void withdraw(double amount) {
+		balance -= amount;
+	}
+	
+	public String toString() {
+		return "New balance: " + String.format("%.2f", balance); 
+	}
+	
+
+
+	
+	
+	
 }
